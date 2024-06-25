979b971da9ed651a2284dc17d7eda5e0afba6347
Thamires Cavalcante <thamires.araujo1996@gmail.com>
2024-06-25 16:06:23 -0300

    cookbook 4

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src= "https://organicsnewsbrasil.com.br/wp-content/uploads/2015/10/natureza.jpg" alt="Imagem Tela Inicial"  className='img'/>
+           
+        </>
+    );
+}
+
+export default Home;
